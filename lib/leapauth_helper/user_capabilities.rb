2be1a821518d331704e3a7d05645dfd882d134c9
Ayumi Yu <ayu.yua@gmail.com>
2012-11-10 09:12:57 -0800

    Replace roles with flags

--- lib/leapauth_helper/user_capabilities.rb
+++ lib/leapauth_helper/user_capabilities.rb
@@ -1,14 +1,15 @@
 module LeapauthHelper
   module UserCapabilities
-    SharedRoles = [ :super_admin, :admin, :representative, :developer, :preorder_admin ]
+    Flags = [ :super_admin, :admin, :representative, :preorder_admin, :developer ]
+    EmployeeFlags = [ :super_admin, :admin, :representative, :preorder_admin ]
 
     def capabilities
       @capabilities ||= begin
         cap = []
         cap << 'blog_writer' if respond_to?(:blog_writer?) && blog_writer?
-        cap << 'dev_access' if respond_to?(:dev_terms?) && dev_terms?
-        cap << 'employee' if respond_to?(:email) && email[/@leapmotion.com$/]
-        cap << 'admin' if respond_to?(:admin?) && admin?
+        cap << 'dev_access' if respond_to?(:is_developer?) && respond_to?(:dev_terms) && is_developer? && dev_terms?
+        cap << 'employee' if respond_to?(:lm) && lm?
+        cap << 'admin' if respond_to?(:is_admin?) && is_admin?
         cap
       end
     end
