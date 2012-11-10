d135c436224de291af475872db2539e29c8e1669
Josh Hull <jhull@leapmotion.com>
2012-11-10 12:07:00 -0800

    remove blog writer

--- lib/leapauth_helper/user_capabilities.rb
+++ lib/leapauth_helper/user_capabilities.rb
@@ -6,7 +6,6 @@ module LeapauthHelper
     def capabilities
       @capabilities ||= begin
         cap = []
-        cap << 'blog_writer' if respond_to?(:blog_writer?) && blog_writer?
         cap << 'dev_access' if respond_to?(:is_developer?) && respond_to?(:dev_terms) && is_developer? && dev_terms?
         cap << 'employee' if respond_to?(:lm) && lm?
         cap << 'admin' if respond_to?(:is_admin?) && is_admin?
