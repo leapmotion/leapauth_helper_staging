8e23d2cb02da4c03e24e1e05309a130503fee8a8
Ayumi Yu <ayu.yua@gmail.com>
2012-11-10 14:25:30 -0800

    Add is_lm to flags

--- lib/leapauth_helper/user_capabilities.rb
+++ lib/leapauth_helper/user_capabilities.rb
@@ -1,13 +1,13 @@
 module LeapauthHelper
   module UserCapabilities
-    Flags = [ :is_super_admin, :is_admin, :is_representative, :is_preorder_admin, :is_developer ]
-    EmployeeFlags = [ :is_super_admin, :is_admin, :is_representative, :is_preorder_admin ]
+    Flags = [ :is_super_admin, :is_admin, :is_representative, :is_preorder_admin, :is_lm, :is_developer ]
+    EmployeeFlags = [ :is_super_admin, :is_admin, :is_representative, :is_preorder_admin, :is_lm ]
 
     def capabilities
       @capabilities ||= begin
         cap = []
         cap << 'dev_access' if respond_to?(:is_developer?) && respond_to?(:dev_terms) && is_developer? && dev_terms?
-        cap << 'employee' if respond_to?(:lm) && lm?
+        cap << 'employee' if respond_to?(:is_lm) && is_lm?
         cap << 'admin' if respond_to?(:is_admin?) && is_admin?
         cap
       end
