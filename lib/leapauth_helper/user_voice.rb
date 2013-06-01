2ef056d7775a3d4c8caff27d7533dc7486b24d4a
Todd Eichel <todd@toddeichel.com>
2013-05-31 18:03:30 -0700

    Fall back to RACK_ENV if RAILS_ENV is not set.

--- lib/leapauth_helper/user_voice.rb
+++ lib/leapauth_helper/user_voice.rb
@@ -8,7 +8,7 @@ module LeapauthHelper
       raise "Must pass User ID, email, and display name to generate a UserVoice SSO token." unless [user_id, email, display_name].all?(&:present?)
       
       # This is the data sent to UserVoice encrypted inside the token.
-      options = { :guid => "#{ENV['RAILS_ENV']}-#{user_id}", :email => email, :display_name => display_name }
+      options = { :guid => "#{ENV['RAILS_ENV'] || ENV['RACK_ENV']}-#{user_id}", :email => email, :display_name => display_name }
       # TODO: Do we want auth tokens to expire?
       # options.merge!({ :expires => (Time.zone.now.utc + 5 * 60).to_s })
 
