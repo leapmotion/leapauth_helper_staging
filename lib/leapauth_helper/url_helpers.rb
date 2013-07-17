a7d31a062763ae8ec014307f17aaff29fd7194b2
Paul Raythattha <mahipalr@gmail.com>
2013-07-16 20:56:34 -0700

    Added helper method to generate URLs to WWW (i.e. Leapweb).

--- lib/leapauth_helper/url_helpers.rb
+++ lib/leapauth_helper/url_helpers.rb
@@ -16,6 +16,13 @@ module LeapauthHelper
         url = "#{scheme}://#{LeapauthHelper.config.auth_host}#{path}"
         opts.empty? ? url : "#{url}?#{Rack::Utils.build_query(opts)}"
       end
+
+      def home_url(path, opts = {})
+        # For some reason, config.home has the scheme baked in, but the others don't.
+        # Just in case changing it would be a breaking change, I won't change it. But... WTF.
+        url = "#{LeapauthHelper.config.home}#{path}"
+        opts.empty? ? url : "#{url}?#{Rack::Utils.build_query(opts)}"
+      end
     end
   end
 end
