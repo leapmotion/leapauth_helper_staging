2ca907c94789f77ebe9dca764da8be6db439f83c
Rodrigo Vanegas <rodrigo@blazingcloud.net>
2013-08-13 14:15:57 -0700

    support cluster env and add a few url gens

--- lib/leapauth_helper/config.rb
+++ lib/leapauth_helper/config.rb
@@ -5,7 +5,6 @@ module LeapauthHelper
   end
 
   # TODO: When we bump the major version, remove the scheme that is hard-coded in the values for the "home" key here.
-
   DEFAULT_CONFIG = {
     'development' => {
       "auth_host"          => "local.leapmotion:3010",
@@ -13,7 +12,8 @@ module LeapauthHelper
       "home"               => "local.leapmotion:3000",
       "cookie_auth_key"    => "_dev_auth",
       "transactions_host"  => "local.leapmotion:5001",
-      "airspace_host"      => "local.leapmotion:5002"
+      "airspace_host"      => "local.leapmotion:5002",
+      "developer_host"     => "local.leapmotion:4000"
     },
     'test' => {
       "auth_host"          =>  "test.leapmotion:1234",
@@ -21,7 +21,8 @@ module LeapauthHelper
       "home"               =>  "test.leapmotion:3000",
       "cookie_auth_key"    =>  "_test_auth",
       "transactions_host"  =>  "test.leapmotion",
-      "airspace_host"      =>  "test.leapmotion"
+      "airspace_host"      =>  "test.leapmotion",
+      "developer_host"     =>  "test.leapmotion"
     },
     'staging' => {
       "auth_host"          =>  "leap:L4!!pStag0ing@central-stage.leapmotion.com",
@@ -29,7 +30,8 @@ module LeapauthHelper
       "home"               =>  "leapweb-stage7.herokuapp.com",
       "cookie_auth_key"    =>  "_stage_auth",
       "transactions_host"  =>  "leap:200hands500fingers@warehouse-stage.leapmotion.com",
-      "airspace_host"      =>  "leap:h0t$tud10d3v@airspace-staging.leapmotion.com"
+      "airspace_host"      =>  "leap:h0t$tud10d3v@airspace-staging.leapmotion.com",
+      "developer_host"     =>  "leap:L4!!pStag0ing@developer-stage2.leapmotion.com"
     },
     'production' => {
       "auth_host"          =>  "central.leapmotion.com",
@@ -38,6 +40,7 @@ module LeapauthHelper
       "cookie_auth_key"    =>  "_auth",
       "transactions_host"  =>  "warehouse.leapmotion.com",
       "airspace_host"      =>  "airspace.leapmotion.com",
+      "developer_host"     =>  "developer.leapmotion.com",
       "mixpanel_token"     =>  "77d363605f0470115eb82352f14b2981",
       "google_property_id" =>  "UA-31536531-1"
     },
@@ -52,7 +55,21 @@ module LeapauthHelper
   }
 
   def self.config
-    config_data = DEFAULT_CONFIG['all'].merge(DEFAULT_CONFIG[ ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'])
+    if cluster_name = ENV['LEAPCLUSTER']
+      raise "cluster name not in [a-z0-9]" unless cluster_name =~ /^[a-z0-9]{1,20}$/
+      cluster = {
+        "auth_host"          =>  "leap:L4!!pStag0ing@central-#{cluster_name}.herokuapp.com",
+        "auth_domain"        =>  "herokuapp.com",
+        "home"               =>  "leapweb-#{cluster_name}.herokuapp.com",
+        "cookie_auth_key"    =>  "_cluster_auth",
+        "transactions_host"  =>  "leap:200hands500fingers@warehouse-#{cluster_name}.herokuapp.com",
+        "airspace_host"      =>  "leap:h0t$tud10d3v@airspace-#{cluster_name}.herokuapp.com",
+        "developer_host"     =>  "leap:L4!!pStag0ing@developer-#{cluster_name}.leapmotion.com"
+      }
+      config_data = DEFAULT_CONFIG['all'].merge(cluster)
+    else
+      config_data = DEFAULT_CONFIG['all'].merge(DEFAULT_CONFIG[ ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'])
+    end
     @@config ||= Config.new(config_data)
   end
 
