9fb534ad8f19d48bca8f31789eedb90924289cc6
Steven Peng <speng@leapmotion.com>
2013-12-06 13:31:39 -0800

    Mock dataLayer for development

--- lib/leapauth_helper/google_tag_manager.rb
+++ lib/leapauth_helper/google_tag_manager.rb
@@ -1,16 +1,17 @@
 module LeapauthHelper
   class GoogleTagManager
     def self.render_init
-      return "" unless LeapauthHelper.config.gtm_container_id.present?
-
-      # Google Tag Manager
-      "<noscript><iframe src='//www.googletagmanager.com/ns.html?id=#{LeapauthHelper.config.gtm_container_id}'
-      height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','#{LeapauthHelper.config.gtm_container_id}');</script>".html_safe
+      if LeapauthHelper.config.gtm_container_id.present?
+        "<noscript><iframe src='//www.googletagmanager.com/ns.html?id=#{LeapauthHelper.config.gtm_container_id}'
+        height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{LeapauthHelper.config.gtm_container_id}');</script>".html_safe
+      else
+        "<script>dataLayer={push:function(e){console.log('dataLayer.push() called:');console.log(e);}};</script>"
+      end
     end
   end
 end
