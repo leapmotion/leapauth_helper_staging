26432394e4cbeb6b604f7af8bbef6d8a8f3b0f52
Paul Raythattha <mahipalr@gmail.com>
2014-02-14 16:11:08 -0800

    Fix whitespace in URL generators spec file.

--- spec/url_generators_spec.rb
+++ spec/url_generators_spec.rb
@@ -1,27 +1,27 @@
 require 'spec_helper'
 
 describe LeapauthHelper::UrlGenerators do
-  let(:controller) { DummyController.new }  
+  let(:controller) { DummyController.new }
   let(:expected_redirect){ CGI.escape('http://local.leapmotion:4000/some-page?a=1&b=2') }
   before do
-    controller.stub :request => stub(:protocol => "http://", 
-                                     :host_with_port => "local.leapmotion:4000", 
+    controller.stub :request => stub(:protocol => "http://",
+                                     :host_with_port => "local.leapmotion:4000",
                                      :fullpath => "/some-page?a=1&b=2")
 
   end
-  
+
   describe "#auth_destroy_session_url method" do
     it "returns the URL" do
       expect( "http://#{LeapauthHelper.config.auth_host}/users/sign_out?_r=#{expected_redirect}").to eql controller.auth_destroy_session_url
     end
   end
-  
+
   describe "#auth_sign_out_url method" do
     it "returns the URL" do
       expect( "http://#{LeapauthHelper.config.auth_host}/users/sign_out?_r=#{expected_redirect}").to eql controller.auth_sign_out_url
     end
   end
-  
+
   describe "#auth_sign_up_url method" do
     it "returns the URL to sign up with a redirect" do
       expect( "http://#{LeapauthHelper.config.auth_host}/users/sign_up?_r=#{expected_redirect}").to eql controller.auth_sign_up_url
@@ -33,7 +33,7 @@ describe LeapauthHelper::UrlGenerators do
       expect( "http://#{LeapauthHelper.config.auth_host}/users/sign_in?_r=#{expected_redirect}").to eql controller.auth_sign_in_url
     end
   end
-  
+
   describe "#auth_create_session_url method" do
     it "returns the URL with a redirect URL" do
       expect( "http://#{LeapauthHelper.config.auth_host}/users/sign_in?_r=#{expected_redirect}").to eql controller.auth_create_session_url
@@ -44,13 +44,13 @@ describe LeapauthHelper::UrlGenerators do
     it "returns the URL to the admin users index page" do
       expect( "http://#{LeapauthHelper.config.auth_host}/admin/users").to eql controller.auth_admin_users_url
     end
-  end  
+  end
 
   describe "#auth_admin_user_url method" do
     it "returns the URL to the admin view of a specific user page" do
       expect( "http://#{LeapauthHelper.config.auth_host}/admin/users/202").to eql controller.auth_admin_user_url(202)
     end
-  end  
+  end
 
   describe "#auth_user_account_url method" do
     it "returns the URL to the account page" do
@@ -62,13 +62,13 @@ describe LeapauthHelper::UrlGenerators do
     it {
       expect(controller.auth_forgot_password_url).to eql "http://#{LeapauthHelper.config.auth_host}/users/password/new"
     }
-  end    
+  end
 
   describe '#auth_require_username_url' do
     it {
       expect(controller.auth_require_username_url).to eql "http://#{LeapauthHelper.config.auth_host}/users/developer"
     }
-  end    
+  end
 
   describe '#auth_require_username_url' do
     it {
